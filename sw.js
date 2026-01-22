89190d7004dc1c130eb609f21d9fd1f200a7856a
Rajatalluri91 <talluriraja91@gmail.com>
2026-01-23 00:33:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
-self.addEventListener('install', event => {
-  console.log('[SW] Installed');
+const CACHE_NAME = "boui-cache-v2"; // version change VERY IMPORTANT
+
+self.addEventListener("install", event => {
+  console.log("[SW] Installed");
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  console.log('[SW] Activated');
-  return self.clients.claim();
+self.addEventListener("activate", event => {
+  console.log("[SW] Activated");
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("[SW] Deleting old cache:", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
